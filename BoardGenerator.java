1c9125a55e6c3125f6f2c7c8ed96512d80cb2052
daveromana2 <33032630+daveromana2@users.noreply.github.com>
2018-01-16 13:21:29 +0100

    Add files via upload

---
+++ BoardGenerator.java
@@ -0,0 +1,55 @@
+package core;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import core.Board;
+
+/**
+ * Classe relativa al BoardGenerator
+ * @author Mina
+ *
+ */
+
+public class BoardGenerator {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		Board[] boardCollection = new Board[200];
+		//Generate boards (9 dead fields each, no guarantee that in terms of 
+		//duplicates).
+		int valueboardGB1 = 1;
+		boolean valueboardGB2 = true;
+		int valueboardGB3 = 3;
+		for(int i = 0; i < 200; ++i) {
+			boardCollection[i] = new Board(valueboardGB1, valueboardGB2, valueboardGB3);
+		}
+
+		//Save to the file.
+		FileOutputStream fos = new FileOutputStream("200_boards_3.sav");
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		try {
+			
+			oos.writeObject(boardCollection);
+			
+		
+		} catch(Exception e) {
+			System.err.println("Error occured during saving.");
+			System.out.println("Something was wrong");
+		}finally {
+	           if (oos != null) {
+	               try {
+	                 oos.close (); 
+	               } catch (java.io.IOException e3) {
+	                 System.out.println("I/O Exception");
+	               }
+	}
+		}
+	}
+	}
+
